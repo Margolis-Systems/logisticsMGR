e15d33d94f840372c53e2d43716a0d058fb8ab2c
Margolis-Systems <117809332+Margolis-Systems@users.noreply.github.com>
2024-10-21 17:47:32 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,7 +2,9 @@
 server_port = 8000
 
 # Mongo configs
-con_str = 'mongodb://localhost:27017/'
+# con_str = 'mongodb://localhost:27017/'
+import os
+con_str = os.environ.get("MONGO_URI")
 db = 'Logistics'
 users_col = 'users'
 docs_col = 'docs'
